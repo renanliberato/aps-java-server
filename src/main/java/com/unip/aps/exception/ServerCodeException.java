b20fabe3307a6a360ea5c495c535686aec2bfe4c
Renan Liberato <renan.libsantana@gmail.com>
2017-05-05 20:11:45 -0300

    Specific Server exception

---
+++ src/main/java/com/unip/aps/exception/ServerCodeException.java
@@ -0,0 +1,12 @@
+package com.unip.aps.exception;
+
+/**
+ * Created on 5/5/2017.
+ *
+ * @author Renan Liberato Sant'Ana <renan.libsantana@gmail.com>
+ */
+public class ServerCodeException extends Exception {
+    public ServerCodeException(String message) {
+        super(message);
+    }
+}
