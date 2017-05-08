6db91871b11b717ba5e3e21b74503127783094b8
Renan Liberato <renan.libsantana@gmail.com>
2017-05-08 06:21:35 -0300

    Implementação de porta dinâmica
    Ao inicializar o servidor, é necessário agora que o usuário informe a porta a ser escutada como parâmetro. Exemplo:
    
    java -jar aps-server.jar 8080

--- src/main/java/com/unip/aps/App.java
+++ src/main/java/com/unip/aps/App.java
@@ -4,6 +4,7 @@ import com.unip.aps.core.Persistence;
 import com.unip.aps.service.ServerHandler;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created on 2/5/2017.
@@ -14,14 +15,20 @@ public class App {
 
     public static void main(String[] args) {
         try {
+            if (args.length != 1) {
+                throw new Exception("Falta informar a porta como argumento");
+            }
             Persistence.getEntityManager();
             System.out.println("DB iniciado");
             // Inicializa a aplicação na porta passada.
-            int port = 666;
+            int port = Integer.valueOf(args[0]);
+
             System.out.println("Servidor escutando a porta "+port);
             ServerHandler.init(port);
         } catch (IOException e) {
             System.out.println("Não foi possível inicializar o servidor.");
+        } catch (Exception e) {
+            System.out.println("Falta informar a porta como argumento");
         }
     }
 }
